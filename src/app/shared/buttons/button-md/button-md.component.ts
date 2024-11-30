fc8895d3b78ca441309c23e0e5b401c583571a88
Miriam Abaitua <miriam.abaitua@seidor.com>
2024-11-30 11:07:03 +0100

    paso de src por variables en buttonMd

--- src/app/shared/buttons/button-md/button-md.component.ts
+++ src/app/shared/buttons/button-md/button-md.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button-md',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './button-md.component.scss'
 })
 export class ButtonMdComponent {
-
+  @Input() cssClassName: string="";
 }
