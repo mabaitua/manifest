82d447f46caf2e7dab902c6584b0ff09469b758b
Miriam Abaitua <miriam.abaitua@seidor.com>
2024-12-01 12:17:29 +0100

    estilos component hero, header y nav

--- src/app/shared/hero/hero.component.ts
+++ src/app/shared/hero/hero.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
+import { ButtonLgComponent } from "../buttons/button-lg/button-lg.component";
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, ButtonLgComponent],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss'
 })
