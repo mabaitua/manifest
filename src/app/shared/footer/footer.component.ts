4e65c50147c60b9ab2e866d6b2d983779081bb06
Miriam Abaitua <miriam.abaitua@seidor.com>
2024-12-01 13:03:03 +0100

    estilos footer

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { LinkComponent } from "../link/link.component";
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [LinkComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
